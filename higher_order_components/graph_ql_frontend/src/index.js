4d8509a3b7a1e5d90c011abf03448ae8f70af88b
Costeira <joaocurveira13@gmail.com>
2022-12-14 11:12:40 +0000

    apollo setup

--- higher_order_components/graph_ql_frontend/src/index.js
+++ higher_order_components/graph_ql_frontend/src/index.js
@@ -2,10 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { ApolloClient, InMemoryCache } from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks';
+
+const client = new ApolloClient({
+  link: "http://localhost:4000/",
+  cache: new InMemoryCache()
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>
 );
