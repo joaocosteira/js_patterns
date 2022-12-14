0ce11bfaad9b3d11e69fde0a44cb7df2a6b90097
Costeira <joaocurveira13@gmail.com>
2022-12-14 10:40:34 +0000

    hover hook version

--- higher_order_components/README.md
+++ higher_order_components/README.md
@@ -2,3 +2,6 @@
 
 - Used to reuse the same logic within different components
 - A higher order component is a component that receives another component as a prop.
+- **Problem**
+    * The goal of a higher order component is to define some kind of logic, and them within the higher order component we call/render the component received as an argument/prop. Which allows us to reuse the logic defined within the higher component on these "children" components
+    * Obviously with the introduction of hooks, speacilly custom hooks, we can simply define our hooks and inject them whenever we want, making the usage of HOC kinda redundant.
--- higher_order_components/dogs_with_loader/src/DogImages.js
+++ higher_order_components/dogs_with_loader/src/DogImages.js
@@ -1,19 +1,25 @@
+import { useHover } from './hooks'
 import withLoader from './withLoader'
-import withHover from './withHover'
 
-const DogImages = (props) => (
 
-    <div {...props}>
-        { props.hovering && <div id="hover">Hovering!</div>}
-        <div id="list">
-            {
-                props.data.message.map((dog,i) => (
-                    <img src={dog} alt={`Dog #${i}`} key={i} />
-                ))
-            }
+const DogImages = (props) => {
+    
+    const [hoverRef,hovering] = useHover();
+
+    return (
+
+        <div ref={hoverRef} {...props}>
+            { hovering && <div id="hover">Hovering!</div>}
+            <div id="list">
+                {
+                    props.data.message.map((dog,i) => (
+                        <img src={dog} alt={`Dog #${i}`} key={i} />
+                    ))
+                }
+            </div>
         </div>
-    </div>
 
-)
+    )
+}
 
-export default withHover(withLoader(DogImages,"https://dog.ceo/api/breed/labrador/images/random/6"))
+export default withLoader(DogImages,"https://dog.ceo/api/breed/labrador/images/random/6")
